25a92faded68732362c6cf1b74ff8b5e8a0225bf
Oleg Grigoriev <go.vasac@gmail.com>
2013-12-26 11:57:50 +0400

    errors\InvalidFormat: Invalid format of callback

---
+++ errors/InvalidFormat.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @package axy\callbacks
+ */
+
+namespace axy\callbacks\errors;
+
+/**
+ * Invalid format of callback
+ *
+ * @author Oleg Grigoriev <go.vasac@gmail.com>
+ */
+class InvalidFormat extends \axy\errors\InvalidConfig implements Error
+{
+    /**
+     * Constructor
+     *
+     * @param string $errmsg [optional]
+     * @param \Exception $previous [optional]
+     * @param mixed $thrower [optional]
+     */
+    public function __construct($errmsg = null, \Exception $previous = null, $thrower = null)
+    {
+        parent::__construct('Callback', $errmsg, 0, $previous, $thrower);
+    }
+}
