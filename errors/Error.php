002e79a30f9e75e670c74aec4be6cf342b3de5b6
Oleg Grigoriev <go.vasac@gmail.com>
2013-12-26 11:57:34 +0400

    errors\Error: The basic error of axy/callbacks

---
+++ errors/Error.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package axy\callbacks
+ */
+
+namespace axy\callbacks\errors;
+
+/**
+ * The basic error of axy/callbacks
+ *
+ * @author Oleg Grigoriev <go.vasac@gmail.com>
+ */
+interface Error extends \axy\errors\Error
+{
+}
